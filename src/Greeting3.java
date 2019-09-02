cc76b8f52b01fea984035977dba717a982924e35
valerygl <v_lvov@mail.ru>
2019-09-02 09:49:19 +0300

    Vasily hello

--- src/Greeting3.java
+++ src/Greeting3.java
@@ -5,4 +5,8 @@ public class Greeting3 {
     public static void main(String[] args) {
         System.out.println("Greeting 3");
     }
+
+    public static void printGreeting(String input) {
+        System.out.println("Hell " + input);
+    }
 }
--- src/Greeting3Test.java
+++ src/Greeting3Test.java
@@ -1,11 +1,33 @@
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-// qwertyu
+
 
 class Greeting3Test {
 
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    // Write a Greeter class with greet function that receives a name as input and outputs Hello <name>.
+    // The signature of greet should not change throughout the kata.
+    // You are allowed to construct Greeter object as you please.
     @Test
-    void main() {
+    public static void printsHelloName() throws Exception {
+        Greeting3.printGreeting("Vasily");
+        assertEquals("Hello Vasily", outContent.toString());
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
     }
 }
